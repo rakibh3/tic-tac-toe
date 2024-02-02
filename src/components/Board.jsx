5fd609234c66c1fc3656712c7a183634d3d5fc7e
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-02-02 23:16:34 +0600

    Text Center

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,7 +6,7 @@ function Square({ value, onSquareClick }) {
     <>
       <button
         onClick={onSquareClick}
-        className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg font-bold"
+        className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg font-bold text-center"
       >
         {value}
       </button>
